f5aff3d814b1f1c4591ebdad12f7ad0e5d20587d
slothgina <gina.slothstudio@zohomail.com>
2026-05-30 20:57:59 -0400

    away with the s

--- SlothSecIpCheckerWeb/obj/Debug/net10.0/SlothSecIpCheckerWeb.AssemblyInfo.cs
+++ SlothSecIpCheckerWeb/obj/Debug/net10.0/SlothSecIpCheckerWeb.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SlothSecIpCheckerWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d43b050374821e634a00cb0967c3e246cbfa2c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b25dd6447114a75589072c0d8a1eb7afbaac7e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("SlothSecIpCheckerWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SlothSecIpCheckerWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
